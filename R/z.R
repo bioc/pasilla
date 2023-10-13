85e44be2aec0f1c2120378d4ae05be8db4bddd9c
Reyes Quiroz, Alejandro <alejandro.reyes_quiroz@novartis.com>
2023-10-13 14:23:20 +0200

    Removed build warning; documented object.

---
+++ R/z.R
@@ -0,0 +1,11 @@
+#' A DEXSeqDataSet containing pasilla exon-level counts
+#'
+#' A DEXSeqDataSet containing exon level counts of the pasilla KO
+#' experiment.
+#'  
+#' @docType data
+#' @keywords datasets
+#' @name dxd
+#' @usage data(pasillaDEXSeqDataSet)
+#' @format A DEXSeqDataSet
+NULL
